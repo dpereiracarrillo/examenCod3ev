19d9035a9e4767c8262b61fbf294e89933ddeeda
dpereiracarrillo <dpereiracarrillo@atanasoft23.danielcastelao.org>
2015-05-07 09:06:00 +0200

    cree los getters y setters

---
+++ src/superficies/Modelo.java
@@ -0,0 +1,93 @@
+package superficies;
+
+/**
+ *
+ * @author dpereiracarrillo
+ */
+public class Modelo { 
+    
+    float squareArea;
+    float sideLength;
+    float sideHeight;
+    float rectangleArea;
+    float baseLength;
+    float height;
+    float triangleArea;
+    float radius;
+    float circleArea;
+
+    public float getSquareArea() {
+        return squareArea;
+    }
+
+    public void setSquareArea(float squareArea) {
+        this.squareArea = squareArea;
+    }
+
+    public float getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(float sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public float getSideHeight() {
+        return sideHeight;
+    }
+
+    public void setSideHeight(float sideHeight) {
+        this.sideHeight = sideHeight;
+    }
+
+    public float getRectangleArea() {
+        return rectangleArea;
+    }
+
+    public void setRectangleArea(float rectangleArea) {
+        this.rectangleArea = rectangleArea;
+    }
+
+    public float getBaseLength() {
+        return baseLength;
+    }
+
+    public void setBaseLength(float baseLength) {
+        this.baseLength = baseLength;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getTriangleArea() {
+        return triangleArea;
+    }
+
+    public void setTriangleArea(float triangleArea) {
+        this.triangleArea = triangleArea;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public float getCircleArea() {
+        return circleArea;
+    }
+
+    public void setCircleArea(float circleArea) {
+        this.circleArea = circleArea;
+    }
+    
+    
+    
+}
